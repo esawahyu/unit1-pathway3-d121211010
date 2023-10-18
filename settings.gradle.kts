d8d5845036a7ac691042f1e5f519d803e8232c2d
esawahyu <esawahyu861@gmail.com>
2023-10-18 20:40:49 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "unit1-pathway3-d121211010"
+include(":app")
+include(":app")
+include(":app")
